ddd0df46ee9043184f8e5430648c5d04edc1ad8c
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 19:17:03 +0330

    Add files via upload

---
+++ Example 16-7/Example 16-7/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Example_16_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBarRed = new System.Windows.Forms.TrackBar();
+            this.trackBarGreen = new System.Windows.Forms.TrackBar();
+            this.trackBarBlue = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBarRed
+            // 
+            this.trackBarRed.LargeChange = 1;
+            this.trackBarRed.Location = new System.Drawing.Point(86, 46);
+            this.trackBarRed.Maximum = 255;
+            this.trackBarRed.Name = "trackBarRed";
+            this.trackBarRed.Size = new System.Drawing.Size(300, 45);
+            this.trackBarRed.TabIndex = 0;
+            this.trackBarRed.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarRed.ValueChanged += new System.EventHandler(this.trackBarRed_ValueChanged);
+            // 
+            // trackBarGreen
+            // 
+            this.trackBarGreen.LargeChange = 1;
+            this.trackBarGreen.Location = new System.Drawing.Point(86, 97);
+            this.trackBarGreen.Maximum = 255;
+            this.trackBarGreen.Name = "trackBarGreen";
+            this.trackBarGreen.Size = new System.Drawing.Size(300, 45);
+            this.trackBarGreen.TabIndex = 1;
+            this.trackBarGreen.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarGreen.ValueChanged += new System.EventHandler(this.trackBarGreen_ValueChanged);
+            // 
+            // trackBarBlue
+            // 
+            this.trackBarBlue.LargeChange = 1;
+            this.trackBarBlue.Location = new System.Drawing.Point(86, 148);
+            this.trackBarBlue.Maximum = 255;
+            this.trackBarBlue.Name = "trackBarBlue";
+            this.trackBarBlue.Size = new System.Drawing.Size(300, 45);
+            this.trackBarBlue.TabIndex = 2;
+            this.trackBarBlue.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarBlue.ValueChanged += new System.EventHandler(this.trackBarBlue_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 227);
+            this.Controls.Add(this.trackBarBlue);
+            this.Controls.Add(this.trackBarGreen);
+            this.Controls.Add(this.trackBarRed);
+            this.Name = "Form1";
+            this.Text = "Example 16-7 (TrackBar & ToolTip)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarRed;
+        private System.Windows.Forms.TrackBar trackBarGreen;
+        private System.Windows.Forms.TrackBar trackBarBlue;
+    }
+}
+
---
+++ Example 16-7/Example 16-7/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_16_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------
+        ToolTip toolTipAll = new ToolTip();
+        //-----------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int r, g, b;
+            r = trackBarRed.Value;
+            g = trackBarGreen.Value;
+            b = trackBarBlue.Value;
+            this.BackColor = Color.FromArgb(r,g,b);
+        }
+        //-----------------------------------------------------------
+        private void trackBarRed_ValueChanged(object sender, EventArgs e)
+        {
+            this.OnLoad(new EventArgs());
+            toolTipAll.SetToolTip(trackBarRed, "R="+trackBarRed.Value.ToString());
+        }
+        //-----------------------------------------------------------
+        private void trackBarGreen_ValueChanged(object sender, EventArgs e)
+        {
+            this.OnLoad(new EventArgs());
+            toolTipAll.SetToolTip(trackBarGreen, "G=" + trackBarGreen.Value.ToString());
+        }
+        //-----------------------------------------------------------
+        private void trackBarBlue_ValueChanged(object sender, EventArgs e)
+        {
+            this.OnLoad(new EventArgs());
+            toolTipAll.SetToolTip(trackBarBlue, "B=" + trackBarBlue.Value.ToString());
+        }
+    }
+}
